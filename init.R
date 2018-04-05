78e38ac997d7f24402eca1db12236a9ffc70e27c
Achintya Rao <achintya.rao@cern.ch>
2018-04-05 13:19:12 +1200

    Update packages in init scripts for Docker/Binder
    Signed-off-by: Achintya Rao <achintya.rao@cern.ch>

--- init.R
+++ init.R
@@ -2,19 +2,12 @@
 
 install.packages('here')
 install.packages('revealjs')
-install.packages('kableExtra')
 install.packages('rcrossref')
 install.packages('rio')
 install.packages('tidytext')
+install.packages('ggrepel')
+install.packages('likert')
 
 # Packages that modify RStudio with addins
 
-## From CRAN / MRAN
-install.packages('citr') # Makes it easy to add citations from a bib(la)tex file
-
-## From GitHub
-devtools::install_github('ThinkR-open/remedy')
 devtools::install_github('r-lib/styler')
-devtools::install_github('benmarwick/wordcountaddin', type = 'source',
-                         dependencies = TRUE)
-devtools::install_github('ropenscilabs/gramr')
